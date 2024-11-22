f922e9a802985ed44ca30917b61afc5cecf07083
INYPOLICY <vodopyan175@gmail.com>
2024-11-22 18:01:06 +0300

    Добавлена логика подсчета чисел

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int y = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма: "+(x+y));
+        System.out.println("Разница: "+(x-y));
+        System.out.println("Произведение: "+(x*y));
+        System.out.println("Частное: "+(double)x/y);
     }
 }
